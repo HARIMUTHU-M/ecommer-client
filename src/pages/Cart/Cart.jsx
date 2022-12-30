9a04657c45c71030dcb00b3f938cd6d3ef000847
HARIMUTHU-M <harimuthutce@gmail.com>
2022-12-30 19:33:14 +0530

    ENV files added

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -5,20 +5,20 @@ import { PRODUCTS } from "../../products";
 import CartItem from "./cart-item";
 import Axios from "axios";
 import "./cart.css";
+// import * as env from 'dotenv'
+// require('dotenv').config()
 
 const Cart = () => {
   const { cartItems, getTotalCartAmount } = useContext(ShopContext);
   const navigate = useNavigate();
 
   const checkout = async () => {
-    Axios.post("http://localhost:3001/checkout", { items: cartItems }).then(
-      (res) => {
-        // console.log(res.data.url);
-        if (res.data.url) {
-          window.location.assign(res.data.url);
-        }
+    const baseUrl = process.env.REACT_APP_URL || "http://localhost:3001";
+    Axios.post(baseUrl + "/checkout", { items: cartItems }).then((res) => {
+      if (res.data.url) {
+        window.location.assign(res.data.url);
       }
-    );
+    });
   };
 
   return getTotalCartAmount() === 0 ? (
